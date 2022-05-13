8be983e5f65c9889d602109a51447c9ed2e31010
Joel Drake <joel.drake@ica.se>
2022-05-13 11:37:19 +0200

    upgraded svelte

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /// <reference types="@sveltejs/kit" />
+
+declare namespace App {
+	interface Calendar {
+		[key: number]: any;
+	}
+}
--- src/lib/dateHelper.ts
+++ src/lib/dateHelper.ts
@@ -1,14 +1,16 @@
-export function getDaysInMonth(month, year) {
+export const dateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+
+export function getDaysInMonth(month: number, year: number) {
 	return new Date(year, month + 1, 0).getDate();
 }
 
-export function getDaysBeforeFirstMonday(date) {
+export function getDaysBeforeFirstMonday(date: Date) {
 	const day = date.getDay();
 	return day - (day === 0 ? -6 : 1);
 }
 
-export function getDateFriendly(date) {
-	let dateOptions = {
+export function getDateFriendly(date: Date) {
+	const dateOptions: Intl.DateTimeFormatOptions = {
 		day: 'numeric',
 		month: 'short'
 	};
@@ -18,19 +20,19 @@ export function getDateFriendly(date) {
 		.replace('.', '');
 }
 
-export function changeDays(date, days) {
+export function changeDays(date: Date, days: number) {
 	const oneDay = 1000 * 3600 * 24;
 	const newDateInMilliseconds = date.getTime() + days * oneDay;
 
 	return new Date(newDateInMilliseconds);
 }
 
-export function getWeek(date) {
+export function getWeek(date: Date) {
 	date.setHours(0, 0, 0, 0);
 	// Thursday in current week decides the year.
 	date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
 	// January 4 is always in week 1.
-	let week1 = new Date(date.getFullYear(), 0, 4);
+	const week1 = new Date(date.getFullYear(), 0, 4);
 	// Adjust to Thursday in week 1 and count number of weeks from date to week1.
 	return (
 		1 +
